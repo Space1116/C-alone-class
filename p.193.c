a532d787f5e1b84f2e42760eda4a77e3d8f591b5
Space1116 <k39396927@gmail.com>
2026-06-08 23:58:15 +0900

    Update p.193.c

--- p.193.c
+++ p.193.c
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
 ﻿//for문
 #include <stdio.h>
 int main(void)
 {
     int i; //반복횟수를 세기 위한 변수 선언
-    for (i = 0; i < 10; i++) //i가 0일 때부터 9까지(i<10)총 9번 반복해서 i에 1을 더하면서 조건문을 반복해라
+    for (i = 0; i < 10; i++) //i가 0일 때부터 9까지(i<10)총 10번 반복해서 i에 1을 더하면서 조건문을 반복해라
     {
         printf("$"); //결과적으로 "$"가 10번 출력
-=======
-﻿#include <stdio.h>
-int main(void)
-{
-    int i;
-    for (i = 1; i < 10; i++)
-    {
-        printf("$");
->>>>>>> f759c4d0a297e27a4bbe0b6212ec21cc98a6382c
     }
     return 0;
 }
