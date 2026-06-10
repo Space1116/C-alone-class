e97eb584263d685187982aceae37ec40333e3169
Space1116 <k39396927@gmail.com>
2026-06-10 21:57:31 +0900

    updat

--- p.259.c
+++ p.259.c
@@ -1,9 +1,18 @@
 ﻿#include <stdio.h>
+#include <string.h>
 
-int main(void) {
+int main(void)
+{
+	char str1[80], str2[80];
+	char temp[80];
 
-	printf("Hello, World!");
+	printf("두 문자열 입력 : ");
+	scanf_s("%s %s", str1, str2);
+	printf("바꾸기 전 : %s, %s\n", str1, str2);
+	strcpy(temp, str1);
+	strcpy(str1, str2);
+	strcpy(str2, temp);
+	printf("바꾼 후 : %s %s", str1, str2);
 
 	return 0;
 }
-
